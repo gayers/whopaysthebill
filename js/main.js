25c4e0ec3a10eeba2ec52844891b342d27301123
Ayers <gayers@btutilities.com>
2017-08-24 15:02:45 -0500

    Update Files

--- js/main.js
+++ js/main.js
@@ -6,18 +6,33 @@
 		this.addApplicant();
 	};
 
+	this.showList = function(){
+			var parent = document.querySelector('.applicant_list_wrapper');
+			var template = "";
+
+			for(var i = 0; i < applicants.length; i++){
+				template += '<span class="name-tag" data-id="'+ i +'">'+ applicants[i]+'</span>';
+
+			}
+
+			parent.innerHTML = '';
+			parent.insertAdjacentHTML("afterbegin", template);
+			deleteOne();
+
+	};
+
 	this.addApplicant = function(){
 		function generateList(input){
 			var value = input.value;
 
-			if(this.checkValid(value)){
-				applicants.push(value);
+			if(this.checkValid(value.toLowerCase())){
+				applicants.push(value.toLowerCase());
 				input.value = '';
+				showList();
 			}else{
 				alert("Something is wrong!");
 			}
 
-			console.log(applicants);
 		}
 
 		var addBtn = document.querySelector("#add_applicant");
@@ -34,6 +49,23 @@
 			return true;
 		}
 		return false;
+	};
+
+	this.deleteOne = function(){
+		var item = document.querySelectorAll('.name-tag');
+
+		function removeIt(element){
+			var attr = parseInt(element.getAttribute('data-id'));
+			applicants.splice(attr,1);
+
+			showList();
+		}
+
+		for(var i = 0; i < item.length; i++){
+			item[i].addEventListener('click',function(e){
+				removeIt(this);
+			})
+		}
 	};
 
 	this.init();
